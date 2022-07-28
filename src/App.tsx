59a91b465a00a9e6f3c3fb9c08cee6bf29d02693
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-07-29 00:23:15 +0100

    Set up the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import './assets/styles/App.css';
+import Footer from './components/Footer';
+import Game from './components/Game';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="game" element={<Game />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
