ac9b5a3e6875816ec2a933dcf9ca8289525613ee
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-08-01 00:43:20 +0100

    Build and style the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full h-18 bg-secondary px-8 md:px-16 lg:px-36 py-8">
+      <span>© 2022 Abdullahi Muritala </span>
+    </div>
+  );
 };
 
 export default Footer;
