48189e53e9bef927e978bf3c5952e6f8fd1ba0fc
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-08-01 00:42:22 +0100

    Impliment full screen with a fixed footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './assets/styles/App.css';
 import Footer from './components/Footer';
 import Game from './components/Game';
 import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col h-screen">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="game" element={<Game />} />
