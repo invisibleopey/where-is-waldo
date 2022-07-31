fd57e186cf6c6384121b0475d3e5844c5ab2aad5
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-08-01 00:41:30 +0100

    Set customized theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      primary: 'rgba(9, 94, 78, 1)',
+      secondary: 'rgba(255, 148, 172, 0.1)',
+      myWhite: 'rgba(248, 248, 248, 1)',
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
