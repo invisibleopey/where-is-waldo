6987dd9a91dc2b473d96140d58b3c0651b3aed6a
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-08-04 01:47:23 +0100

    Build and style a placeholder game page

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import Waldo from '../assets/images/Waldo.jpg';
+import Odlaw from '../assets/images/Odlaw.jpg';
+import Whitebeard from '../assets/images/Whitebeard.jpg';
+import Beach from '../assets/images/beach main.jpg';
 
 const Game = () => {
-  return <div>Game</div>;
+  return (
+    <div className="px-[30px] md:px-[60px] lg:px-[140px]">
+      <header className="w-full px-8 md:px-16 lg:px-36 py-8 text-center flex justify-between items-center">
+        <button className="bg-primary text-myWhite py-2 px-4 text-sm md:text-base md:py-4 md:px-8">
+          Main
+        </button>
+        <div className="flex gap-3 shrink-[2]">
+          <figure className="myFigure">
+            <img src={Waldo} alt="A close up look of waldo" />
+            <figcaption className="text-xs md:text-base">Waldo</figcaption>
+          </figure>
+          <figure className="myFigure">
+            <img src={Odlaw} alt="A close up look of Odlaw" />
+            <figcaption className="text-xs md:text-base">Odlaw</figcaption>
+          </figure>
+          <figure className="myFigure">
+            <img src={Whitebeard} alt="A close up look of Whitebeard" />
+            <figcaption className="text-xs md:text-base">Whitebeard</figcaption>
+          </figure>
+        </div>
+        <div>00m:00s</div>
+      </header>
+      <section className="mb-[30px] lg:mb-[70px]">
+        <figure className="w-[100%]">
+          <img src={Beach} alt="Locate the three characters on the beach" />
+        </figure>
+      </section>
+      <section>
+        <div className="text-center">
+          <button className="btn">View leaderboards</button>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Game;
