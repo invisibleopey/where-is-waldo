50cb9558e28d890beb87da007f6c0c5fa84714bb
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-08-04 23:26:06 +0100

    Render different image based on link parameter

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,8 +3,30 @@ import Waldo from '../assets/images/Waldo.jpg';
 import Odlaw from '../assets/images/Odlaw.jpg';
 import Whitebeard from '../assets/images/Whitebeard.jpg';
 import Beach from '../assets/images/beach main.jpg';
+import Snow from '../assets/images/snow main.jpg';
+import Space from '../assets/images/space main.jpg';
+import { useParams } from 'react-router-dom';
 
 const Game = () => {
+  let params = useParams();
+  let gameUrl;
+
+  switch (params.gameId) {
+    case 'game1':
+      gameUrl = Beach;
+      break;
+
+    case 'game2':
+      gameUrl = Snow;
+      break;
+
+    case 'game3':
+      gameUrl = Space;
+      break;
+    default:
+      break;
+  }
+
   return (
     <div className="px-[30px] md:px-[60px] lg:px-[140px]">
       <header className="w-full px-8 md:px-16 lg:px-36 py-8 text-center flex justify-between items-center">
@@ -29,7 +51,7 @@ const Game = () => {
       </header>
       <section className="mb-[30px] lg:mb-[70px]">
         <figure className="w-[100%]">
-          <img src={Beach} alt="Locate the three characters on the beach" />
+          <img src={gameUrl} alt="Locate the three characters on the beach" />
         </figure>
       </section>
       <section>
