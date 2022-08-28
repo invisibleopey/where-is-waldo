305e0e6cc02fe9c115953b164d9123b8368c1011
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-08-29 00:23:46 +0100

    Translate the circle by half to the left. Make its size responsive.

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 type Positions = {
-  posX: number;
-  posY: number;
+  boxPosX: number;
+  boxPosY: number;
 };
 
 const Popup = (props: Positions) => {
   return (
     <div
       className=" absolute flex text-xs gap-1"
-      style={{ top: `${props.posY - 12}px`, left: `${props.posX - 12}px` }}
+      style={{ top: `${props.boxPosY}px`, left: `${props.boxPosX}px` }}
     >
-      <div className=" rounded-full w-12 h-12 border-solid border-[#000000] border-2"></div>
+      <div className=" rounded-full w-8 h-8 md:w-12 md:h-12 border-solid border-[#000000] border-2 translate-y-[-50%] translate-x-[-50%]"></div>
       <div className="flex flex-col">
         <button className="option-btns">Waldo</button>
         <button className="option-btns">Odlaw</button>
